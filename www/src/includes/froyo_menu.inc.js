08a51f96301e590874d0865aeb1bbb426948a883
joroni <jraymund.niconi@gmail.com>
2014-02-24 15:36:48 +0800

    inc menu

--- www/src/includes/froyo_menu.inc.js
+++ www/src/includes/froyo_menu.inc.js
@@ -6,11 +6,12 @@
 				$('#preloader').show('slow');
 				$('#status').show('slow');
 				$('#shout-comments').hide();
+				$('#suggestion').hide();
 				node_load(3, {
 					success: function(node) {
 						$('#preloader').fadeOut();
 					  $('#status').fadeOut();
-					  $('#social-comments').show();
+					  $('#social-comments').show('slow');
 					 // alert('Loaded node: ' + node.title);
 					 // $("#title").html(node.title);
 					 // $("#banner").html("<img src='" + node.path+"'/>");
@@ -27,6 +28,19 @@
 					}
 				});
 			}
+			function popupWin1() {
+					node_load(3, {
+								success: function(node) {
+									$('#preloader').fadeOut();
+								  $('#status').fadeOut();
+									//$("#contents").html(data);
+								 // alert('Loaded node: ' + node.title);
+								},
+								error: function(xhr, status, message) {
+								  alert(message)
+								}
+							});
+			}
 		/********************** END SOCIAL PAGE **************************/
 		
 		/********************** SHOUT PAGE **************************/
@@ -35,11 +49,13 @@
 					$('#preloader').show('slow');
 				$('#status').show('slow');
 				$('#social-comments').hide();
+				$('#suggestion').hide();
 				node_load(5, {
 					success: function(node) {
 						$('#preloader').fadeOut();
+						$('#social-comments').hide();
 					  $('#status').fadeOut();
-					 $('#shout-comments').show();
+					 $('#shout-comments').show('slow');
 					 // alert('Loaded node: ' + node.title);
 					 /* $("#title").html('Shout Out!');*/
 					  
@@ -51,5 +67,57 @@
 					}
 				});
 			}
-
+			function popupWin2() {
+					node_load(5, {
+								success: function(node) {
+									$('#preloader').fadeOut();
+								  $('#status').fadeOut();
+									//$("#contents").html(data);
+								 // alert('Loaded node: ' + node.title);
+								},
+								error: function(xhr, status, message) {
+								  alert(message)
+								}
+							});
+			}
       /********************** SHOUT PAGE **************************/
+	  
+	  /********************** WHATS PAGE **************************/
+
+			function listWhats() {
+				$('#preloader').show('slow');
+				$('#status').show('slow');
+				$('#social_share').hide();
+				$('#suggestion').hide();
+				
+				node_load(15, {
+					success: function(node) {
+						$('#preloader').fadeOut();
+					  $('#status').fadeOut();
+					  $('#suggestion').show('slow');
+					  
+						$("#title").html("<img src='"+Drupal.settings.site_path + node.title+"'/>");
+					 // alert('Loaded node: ' + node.title);
+					},
+					error: function(xhr, status, message) {
+					  alert(message)
+					}
+				});
+				
+				
+			}
+
+			function popupWin3() {
+					node_load(9, {
+								success: function(node) {
+									$('#preloader').fadeOut();
+								  $('#status').fadeOut();
+									//$("#contents").html(data);
+								 // alert('Loaded node: ' + node.title);
+								},
+								error: function(xhr, status, message) {
+								  alert(message)
+								}
+							});
+			}
+      /********************** WHATS PAGE **************************/
